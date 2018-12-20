4cc160506210ed8681c8539bf834955b32b7d83c
HoYuanhao <727869774@qq.com>
2018-12-21 00:27:59 +0800

    config

--- application-config/src/main/java/com/open/application/config/App.java
+++ application-config/src/main/java/com/open/application/config/App.java
@@ -3,13 +3,21 @@ package com.open.application.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+/**
+ *
+ * @author HeYuanHao
+ */
 @EnableConfigServer
 @SpringBootConfiguration
 @EnableAutoConfiguration
+@SpringBootApplication
 public class App {
-public static void main(String[] args){
-  SpringApplication.run(App.class,args);
-}
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
 }
