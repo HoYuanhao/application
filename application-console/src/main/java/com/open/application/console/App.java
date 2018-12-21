2d9ba0a4f080cbc4a9aec29420789ecbaef2c09f
HoYuanhao <727869774@qq.com>
2018-12-21 11:45:17 +0800

    hyh

--- application-console/src/main/java/com/open/application/console/App.java
+++ application-console/src/main/java/com/open/application/console/App.java
@@ -2,13 +2,15 @@ package com.open.application.console;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 
 @SpringBootApplication
 public class App {
 
   public static void main(String[] args) {
-    SpringApplication.run(App.class, args);
+
+    new SpringApplicationBuilder(App.class).run(args);
   }
 
 }
---
+++ application-console/src/main/java/com/open/application/console/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.open.application.console.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户登录控制器
+ * @author HeYuanHao
+ */
+@RestController
+public class LoginController {
+
+}
